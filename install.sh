c410054ee581f1fa294e57b223dc806f0f9f1ca8
b0r15.8 <b0r15.8@yandex.ru>
2023-03-04 00:51:41 +0300

    Created an auto-installation script, added minor changes to the xmonad configuration

---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+## Script to automatically install custom graphical environment and repository configurations https://codeberg.org/80r151a/dotfiles
+#
+## It is assumed that the installation will be performed on the OpenSuse LEAP distribution. 
+## But in theory the script can work successfully on other distributions if the variables are changed to valid values for a particular environment
+# 
+## Script created 80r151a
+## https://codeberg.org/80r151a
+## https://github.com/80r151a
+
+
+## VARIABLES ##
+
+# calling the package manager with the install attribute
+installPM="zypper in"
+
+# Xorg server and compatibility metapackage
+xorgPack=xorg-x11-server
+
+# X Window System initializer
+xinitPack=xinit
+
+# GNU make tool
+makePack=make
+
+# GNU Compiler Collection
+gccPack=gcc
+
+# Glasgow Haskell Compiler
+ghcPack=ghc
+
+# Development files for the Core X11 protocol library
+libX11DevPack=libX11-devel
+
+# Development files for the X FreeType library
+libXftDevPack=libXft-devel
+
+# Development files for the X11 Xinerama extension
+libXineramaDevPack=libXinerama-devel
+
+# Development files for the X Resize-Rotate-Reflection library
+libXrandrDevPack=libXrandr-devel
+
+# Development files for the X11 Screen Saver extension library
+libXssDevPack=libXss-devel
+
+# Compositing manager for X servers
+xcompmgrPack=xcompmgr
+
+# Xmobar - minimalistic text based status bar
+xmonadPack=xmobar
+
+# Directory with downloaded dotfiles repository
+dirDotfiles=$HOME/dotfiles
+
+# Directory with xmonad configuration and sources
+dirXmonad=$HOME/.config/xmonad/
+
+# Directory with xmobar configuration
+dirXmobar=$HOME/.config/xmobar/
+
+
+## PREPARATION ##
+
+# Installing the required packages
+sudo $installPM $xorgPack $xinitPack $makePack $gccPack $ghcPack $libX11DevPack $libXftDevPack $libXineramaDevPack $libXrandrDevPack $libXssDevPauck $xcompmgrPack $xmonadPack
+
+# Creating directories for Xmonad and xmobar configurations
+mkdir $dirXmonad $dirXmobar
+
+# Creating a custom bin directory in its home directory
+# It will contain the compiled xmonad executable
+mkdir $HOME/.local/bin
+
+# Copy xmonad and xmonad-contrib repositories to xmonad config directory for build
+git clone https://github.com/xmonad/xmonad $dirXmonad/xmonad
+git clone https://github.com/xmonad/xmonad-contrib $dirXmonad/xmonad-contrib
+
+# Moving xmonad and xmobar config files from dotfiles to config directories
+cp $dirDotfiles/xmonad/xmonad.hs $dirXmonad
+cp $dirDotfiles/xmobar/xmobarrc $dirXmobar
+
+# Installing the stack to build and install the xmonad project 
+curl -sSL https://get.haskellstack.org/ | sh
+
+# Adding the path to the ~/.local/bin directory to the PATH variable via the bashrc configuration file
+# # To run the xmonad executable
+echo -e '\nPATH=$PATH:$HOME/.local/bin\nexport PATH' >> $HOME/.bashrc
+
+# Applying the updated bashrc configuration file
+source $HOME/.bashrc
+
+
+## INSTALL XMONAD ##
+
+# Moving to the directory with the xmonad project (aka xmonad configuration directory) to build and install it 
+cd $dirXmonad
+
+# Project initialization
+stack init
+
+# Installing the project (building and moving the executable to ~/.local/bin)
+stack install
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,3 +1,8 @@
+
+-------------------------------------
+  -------- Imports Modules --------
+-------------------------------------
+
 import XMonad
 import XMonad.Hooks.DynamicLog        (ppSep, ppTitleSanitize, ppCurrent, ppHidden, ppHiddenNoWindows, ppUrgent, ppOrder, ppExtras)
 import XMonad.Hooks.ManageHelpers     (isFullscreen, doFullFloat, isDialog, isInProperty, transience', composeOne, (-?>))
@@ -11,7 +16,12 @@ import XMonad.Layout.ThreeColumns     (ThreeCol(ThreeColMid))
 import XMonad.Hooks.EwmhDesktops      (ewmhFullscreen, ewmh)
 import XMonad.Layout.Fullscreen       (fullscreenFull)
 import XMonad.Layout.Spacing          (smartSpacingWithEdge)
+import XMonad.Hooks.FadeWindows       (Opacity, fadeWindowsLogHook, fadeWindowsEventHook, isUnfocused, opaque, transparency)
 import qualified XMonad.Hooks.ManageDocks as Docks
+
+-------------------------------------
+  ------------ Main ---------------
+-------------------------------------
 
 main :: IO ()
 main = xmonad
@@ -20,12 +30,18 @@ main = xmonad
      . withEasySB (statusBarProp "xmobar" (pure myXmobarPP)) defToggleStrutsKey
      $ myConfig
 
+-------------------------------------
+  ----- Custom Configuration ------
+-------------------------------------
+
 myConfig = def
-    { modMask         = mod4Mask      -- Rebind Mod to the Super key
-    , terminal        = "urxvt"       -- Default terminal
-    , layoutHook      = myLayout      -- Use custom layouts
-    , manageHook      = myManageHook  -- Match on certain windows
-    , borderWidth     = 2
+    { modMask             = mod4Mask                        -- Rebind Mod to the Super key
+    , terminal            = "urxvt"                         -- Default terminal
+    , layoutHook          = myLayout                        -- Use custom layouts
+    , manageHook          = myManageHook                    -- Match on certain windows
+    , borderWidth         = 2
+    , logHook             = fadeWindowsLogHook myFadeHook
+    , handleEventHook     = fadeWindowsEventHook
     }
   `additionalKeysP`
     [ ("M-S-z", spawn "i3lock" )
@@ -33,10 +49,27 @@ myConfig = def
     , ("M-f"  , spawn "firefox" )
     ]
 
+-------------------------------------
+  ------------- Hooks -----------
+-------------------------------------
+
+-------------------------------------
+-- Spawn processes 
+-------------------------------------
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawn "xcompmgr -c &"
+
+-------------------------------------
+-- Compositing hooks 
+-------------------------------------
+
+--- manageHook for working with fullscreen, temporary, dialogs windows and splash screens
 myManageHook = composeAll [ transience', manageWindow, manageOverrides ]
   where manageWindow = composeOne
           [ isFullscreen -?> doFullFloat
-          -- Dialogs (e.g. Ubuntu logout dialog)
+          -- Dialogs (e.g. logout dialog)
           , isDialog -?> doFloat
           -- Splash screens
           , isSplash -?> doIgnore
@@ -46,40 +79,54 @@ myManageHook = composeAll [ transience', manageWindow, manageOverrides ]
           ]
         isSplash = isInProperty "_NET_WM_WINDOW_TYPE" "_NET_WM_WINDOW_TYPE_SPLASH"
 
-myLayout = Docks.avoidStruts $ tiled ||| Mirror tiled ||| fullscreenFull Full ||| threeCol
-  where
-    threeCol =  withSpaces $ magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
-    tiled    = withSpaces $ Tall nmaster delta ratio
-    nmaster  = 1      -- Default number of windows in the master pane
-    ratio    = 1/2    -- Default proportion of screen occupied by master pane
-    delta    = 3/100  -- Percent of screen to increment by when resizing panes
-    withSpaces layout = smartSpacingWithEdge 3 $ layout
 
+--- FadeHook with which inactive tiles become transparent
+myFadeHook :: Query Opacity
+myFadeHook = composeAll [                 opaque
+                        , isUnfocused --> transparency 0.12
+                        ]
+-------------------------------------
+-- Status bar hooks
+-------------------------------------
 
 myXmobarPP :: PP
 myXmobarPP = def
-    { ppSep             = magenta " • "
-    , ppTitleSanitize   = xmobarStrip
-    , ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
-    , ppHidden          = white . wrap " " ""
-    , ppHiddenNoWindows = lowWhite . wrap " " ""
-    , ppUrgent          = red . wrap (yellow "!") (yellow "!")
-    , ppOrder           = \[ws, l, _, wins] -> [ws, l, wins]
-    , ppExtras          = [logTitles formatFocused formatUnfocused]
+    { ppSep               = magenta " • "
+    , ppTitleSanitize     = xmobarStrip
+    , ppCurrent           = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2
+    , ppHidden            = white . wrap " " ""
+    , ppHiddenNoWindows   = lowWhite . wrap " " ""
+    , ppUrgent            = red . wrap (yellow "!") (yellow "!")
+    , ppOrder             = \[ws, l, _, wins] -> [ws, l, wins]
+    , ppExtras            = [logTitles formatFocused formatUnfocused]
     }
   where
-    formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
+    formatFocused         = wrap (white    "[") (white    "]") . magenta . ppWindow
+    formatUnfocused       = wrap (lowWhite "[") (lowWhite "]") . blue    . ppWindow
 
     -- | Windows should have *some* title, which should not not exceed a
     -- sane length.
     ppWindow :: String -> String
-    ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
+    ppWindow              = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
 
     blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta  = xmobarColor "#ff79c6" ""
-    blue     = xmobarColor "#bd93f9" ""
-    white    = xmobarColor "#f8f8f2" ""
-    yellow   = xmobarColor "#f1fa8c" ""
-    red      = xmobarColor "#ff5555" ""
-    lowWhite = xmobarColor "#bbbbbb" ""
+    magenta               = xmobarColor "#ff79c6" ""
+    blue                  = xmobarColor "#bd93f9" ""
+    white                 = xmobarColor "#f8f8f2" ""
+    yellow                = xmobarColor "#f1fa8c" ""
+    red                   = xmobarColor "#ff5555" ""
+    lowWhite              = xmobarColor "#bbbbbb" ""
+
+-------------------------------------
+  ------------ layouts ------------
+-------------------------------------
+
+myLayout = Docks.avoidStruts $ tiled ||| Mirror tiled ||| fullscreenFull Full ||| threeCol
+  where
+    threeCol              = withSpaces $ magnifiercz' 1.3 $ ThreeColMid nmaster delta ratio
+    tiled                 = withSpaces $ Tall nmaster delta ratio
+    nmaster               = 1      -- Default number of windows in the master pane
+    ratio                 = 1/2    -- Default proportion of screen occupied by master pane
+    delta                 = 3/100  -- Percent of screen to increment by when resizing panes
+    withSpaces layout     = smartSpacingWithEdge 3 $ layout
+
