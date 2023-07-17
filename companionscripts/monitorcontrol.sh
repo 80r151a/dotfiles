c54fab6a41fccd0d549b7f8335db23cf1693809d
b0r15.8 <b0r15.8@yandex.ru>
2023-07-17 16:23:32 +0300

    added companionscripts directory

---
+++ companionscripts/monitorcontrol.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+
+declare -a data=$(cat ${HOME}/.cache/companionscripts/videoportstatus)
+echo "${data}"
+
+#function familiar_monitor_setup () 
+#{
+#
+#}
+
+
+#function unknown_monitors_setup ()
+#{
+#
+#}
+
+
+#function built-in_monitor_setup ()
+#{
+#
+#}
